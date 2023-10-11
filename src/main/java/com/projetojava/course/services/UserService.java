3842a46a470414e89c07cb29fdc63d10e4bfd2b7
Luiz Antonio <luizcamargo69@gmail.com>
2023-10-11 15:11:11 -0300

    Exception handling - delete

--- src/main/java/com/projetojava/course/services/UserService.java
+++ src/main/java/com/projetojava/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projetojava.course.entities.User;
 import com.projetojava.course.repositories.UserRepository;
+import com.projetojava.course.services.exceptions.DatabaseException;
 import com.projetojava.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
